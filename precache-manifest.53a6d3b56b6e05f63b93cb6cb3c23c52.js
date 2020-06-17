33a202e5da9140d127d81be246b6bdf101dc7c73
Shuyu <s.zong@student.unsw.edu.au>
2020-06-17 15:14:45 +1000

    Updates

---
+++ precache-manifest.53a6d3b56b6e05f63b93cb6cb3c23c52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fae40fe9d9432982f46019b853660d",
+    "url": "/React_Node_Express_app/index.html"
+  },
+  {
+    "revision": "c92d88d266baf8fa39f2",
+    "url": "/React_Node_Express_app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7ae2a5b9a6d750c681d4",
+    "url": "/React_Node_Express_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c92d88d266baf8fa39f2",
+    "url": "/React_Node_Express_app/static/js/2.41820daa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Node_Express_app/static/js/2.41820daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae2a5b9a6d750c681d4",
+    "url": "/React_Node_Express_app/static/js/main.c1324262.chunk.js"
+  },
+  {
+    "revision": "b88a77b831f66960a242",
+    "url": "/React_Node_Express_app/static/js/runtime-main.929b0124.js"
+  }
+]);
