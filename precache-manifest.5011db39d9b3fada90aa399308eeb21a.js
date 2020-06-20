2f55b3d948508d929e3903a8db0d8fed875d2b70
Shuyu <s.zong@student.unsw.edu.au>
2020-06-20 19:35:07 +1000

    Updates

---
+++ precache-manifest.5011db39d9b3fada90aa399308eeb21a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "365a84bee52188e051cab3054477d1ea",
+    "url": "/React-A-Simple-Shopping-Cart-Model/index.html"
+  },
+  {
+    "revision": "8cb6792b0398b9722d13",
+    "url": "/React-A-Simple-Shopping-Cart-Model/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1b884e3380853afc7ca1",
+    "url": "/React-A-Simple-Shopping-Cart-Model/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "8cb6792b0398b9722d13",
+    "url": "/React-A-Simple-Shopping-Cart-Model/static/js/2.90c8b314.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-A-Simple-Shopping-Cart-Model/static/js/2.90c8b314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b884e3380853afc7ca1",
+    "url": "/React-A-Simple-Shopping-Cart-Model/static/js/main.2e937483.chunk.js"
+  },
+  {
+    "revision": "73341895deee2fe3e1f7",
+    "url": "/React-A-Simple-Shopping-Cart-Model/static/js/runtime-main.eced9d47.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/a-simple-shopping-cart-model/precache-manifest.a9e7d1605902b0792d68410998ac13e7.js"
+  "/React-A-Simple-Shopping-Cart-Model/precache-manifest.5011db39d9b3fada90aa399308eeb21a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/a-simple-shopping-cart-model/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-A-Simple-Shopping-Cart-Model/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
